b316a995ca99290105e4ab9f4e2b5b1bb02d936d
hudua <40040433+hudua@users.noreply.github.com>
2022-10-12 11:47:04 -0400

    Update cli-azcopy.sh

--- automation/cli-azcopy.sh
+++ automation/cli-azcopy.sh
@@ -1,1 +1,4 @@
+#Download and learn about Azcopy here: https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-v10
 
+azcopy login --tenant-id=[tenant-id]
+azcopy copy 'https://datalakestoragesharedtre.dfs.core.windows.net/genomics/<file-name>.csv' './<file-name>.csv'
